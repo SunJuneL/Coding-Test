69a5cbe92ac49772379c8925a638add0d3efd9f5
Sun Joon Lee <115583150+SunJuneL@users.noreply.github.com>
2024-04-01 22:54:05 +0900

    프로그래머스 - 아이스 아메리카노 추가

---
+++ Programmers/IceAmericano/IceAmericano.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Language
+//
+//  Created by 이선준 on 1/23/24.
+//
+
+import Foundation
+
+func solution(_ money:Int) -> [Int] {
+    return [money / 5500, money % 5500]
+}

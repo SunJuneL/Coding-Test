eedcbbc080d5343e169861cef177a4c7126a7f28
Sun Joon Lee <115583150+SunJuneL@users.noreply.github.com>
2024-08-08 13:02:57 +0900

    수정) [BOJ-2446 별찍기 - 9] Swift파일 수정
    - Swift 파일의 내용이 이전에 풀었던 코테 문제로 업로드 되어있어 수정.

--- BOJ/BOJ-2446/BOJ-2446.swift
+++ BOJ/BOJ-2446/BOJ-2446.swift
@@ -1,6 +1,28 @@
 import Foundation
 
-// 입력이 있는 동안, 입력 그대로 출력하는 걸 반복함
-while let input = readLine() {
-    print(input)
+// N을 입력 받음
+let N = Int(readLine() ?? "1") ?? 1
+
+// 별 문자열 초기화
+var stars = ""
+for _ in 1 ... (2 * N - 1) {
+    stars.append("*")
+}
+
+// 규칙에 따라 2 * N - 1 개의 줄 출력
+for i in 1 ... (2 * N - 1) {
+    // 첫 줄부터 N 줄까지, 별을 2개 줄이고 공백을 1개 추가
+    if 1 < i && i <= N {
+        stars.removeLast()
+        stars.removeLast()
+        stars = " " + stars
+    }
+    // N 줄 이후, 공백을 1개 줄이고 별 2개 추가
+    if N < i {
+        stars.removeFirst()
+        stars.append("*")
+        stars.append("*")
+    }
+    // 줄을 출력한다.
+    print(stars)
 }
